53d440f02f7432867459dab45ea43bdca2cf0202
Supren <52043489+Supren@users.noreply.github.com>
2020-06-04 21:34:48 +0200

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,98 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { firebase } from "@firebase/app";
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/login",
+        name: "Login",
+        
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/Login.vue"),
+    },
+    {
+        path: "/signup",
+        name: "SignUp",
+        
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/SignUp.vue"),
+    },
+    {
+        path: "/results",
+        name: "Results",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/Results.vue"),
+    },
+    {
+        path: "/recipe",
+        name: "Recipe",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/Recipe.vue"),
+    },
+    {
+        path: "/",
+        name: "Home",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/Home.vue"),
+    },
+    {
+        path: "/favourites",
+        name: "Favourites",
+        
+        component: () =>
+            import("../views/Favourites.vue"),
+    },
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!firebase.auth().currentUser) {
+            next({
+                path: '/login',
+                query: {
+                    redirect: to.fullPath
+                }
+            })
+        } else {
+            next();
+        }
+    } else if (to.matched.some(record => record.meta.requiresGuest)) {
+        if (firebase.auth().currentUser) {
+            next({
+                path: '/',
+                query: {
+                    redirect: to.fullPath
+                }
+            })
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+})
+
+export default router;
